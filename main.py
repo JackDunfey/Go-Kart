a5102f4b11e4ce2306fb6e0bbadff2be1245e8bf
Jack <62919016+pyjshtml@users.noreply.github.com>
2022-03-30 10:09:55 -0400

    Improved dashboard

--- main.py
+++ main.py
@@ -31,13 +31,27 @@ class Robot:
             self.PDM.setSwitchableChannel(False)
 
     def updateDisplay(self):
-        frame = np.zeros(shape=(1920, 1080, 3))
-        texts = [spstr(int(self.chassis.get_speed() + 0.5)) + " MPH"eed, f"Parking Brake: {self.chassis.parking}", f"Cruising @ {self.cruise_speed} MPH" if self.cruising else "Not cruising"]
+        WIDTH, HEIGHT = 1920, 1080
+        frame = np.zeros(shape=(WIDTH, HEIGHT, 3))
+
+        # Display text
+        texts = [f"Parking Brake: {self.chassis.parking}", f"Cruising @ {self.cruise_speed} MPH" if self.cruising else "Not cruising"]
         y = 0
         for text in texts:
             w, h = cv2.getTextSize(text, Robot.font, 1, 1)
-            y += 2*(h+Robot.TEXT_PADDING)
+            y += 2*(h+Robot.TEXT_PADDING) # May need to be adjusted after font correction
             cv2.putText(frame, text, (0, y), Robot.font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+        
+        # Speedometer
+        speed_str = str(int(self.chassis.get_speed() + 0.5)) + " MPH"
+        w, h = cv2.getTextSize(text, Robot.font, 1, 1)
+        cv2.putText(frame, text, ((WIDTH-w)//2, (HEIGHT+h)//2), Robot.font, 5, (255, 255, 255), 5, cv2.LINE_AA)
+
+        # Safe mode indicator
+        r = 200
+        cv2.rectangle(frame, (WIDTH - r, 0), (WIDTH, 30), (0, 255, 0) if self.chasss.safe_mode else (0, 0, 255), -1)
+
+        # Display output
         cv2.imshow("Speedometer", frame)
         cv2.waitKey(1)
 
